b1a9af366917a2f9d7bb0b560325384491e38ed0
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-10-17 14:31:18 +1300

    installed the react app for the client side

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     jwtsecret: "supersecret",
+    StripeAPIKey: ""
 }
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,16 +1,27 @@
 const OrderModel = require("../models/OrderModel");
 const CartModel = require("../models/CartModel");
 const UserModel = require("../models/UserModel");
+const config = require("../../config");
+const stripe = require("stripe")(config.StripeAPIKey);
 module.exports = {
   checkout: async (request, response, next) => {
     try {
       const userId = request.params.id;
-      const {source} = request.body;
+      const { source } = request.body;
       const cart = await CartModel.findOne({ userId });
-      const user = await UserModel.findOne({_id: userId});
+      const user = await UserModel.findOne({ _id: userId });
       const email = user.email;
-      if(!cart) {
-        return response.status(200).json({message: "Cart is empty"})
+      if (!cart) {
+        return response.status(200).json({ message: "Cart is empty" });
+      }
+      const charge = await stripe.charges.create({
+        amount: cart.bill,
+        currency: "NZD",
+        source: source,
+        receipt_email: email,
+      });
+      if(!charge) {
+        throw Error('Payment failed');
       }
       const createOrder = new OrderModel({
         userId: cart.userId,
@@ -18,6 +29,7 @@ module.exports = {
         bill: cart.bill,
       });
       const order = await createOrder.save();
+      await CartModel.findOneAndDelete({userId});
       return response.status(200).json(order);
     } catch (error) {
       next(error);
@@ -26,8 +38,8 @@ module.exports = {
   getOrders: async (request, response, next) => {
     const { id } = request.params;
     const orders = await OrderModel.find({});
-    if(!orders) {
-        return response.status(200).json({message: "You have no orders"});
+    if (!orders) {
+      return response.status(200).json({ message: "You have no orders" });
     }
     return response.status(200).json(orders);
   },
